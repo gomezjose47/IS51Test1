58a06315e9420616beeba6e0cda6ae0a36a09034
Gomezjose47 <gomezjose47@mail.fresnostate.edu>
2020-11-12 17:00:22 -0800

    structured-english

--- exam_1.py
+++ exam_1.py
@@ -1,4 +1,14 @@
 """
+program will compare and then determine which pay option is better
+First option will pay 100 dollars per day for 10 days. The second option is 
+1 dollar per day, and it will be doubled every day for 10 days.
+There will be 2 functions, The first will calculate the pay rate for the first option
+Function 2 will calculate the amount of the second function.
+Function1 will return $100 * 10 days.
+Function2 will be a loop function and it will loop 10 times, each time doubling the amount and adding the amount to the total.
+If Option1 is better we will output "Option1 is better"
+If Option2 is better, we will output "Option2 is better"
+If Option1 and Option2 are equal, we will output, "Option1 and Option2 have the same payrate"
 
 
 """
